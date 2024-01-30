840228c1b5d3d910b9cbc1e8f2a9ecea425db9e5
Donald Roessler <me@donaldroessler.com>
2024-01-30 12:35:42 -0800

    Add wowhead integration

--- react-vite/src/components/WantedMounts/MountListing.jsx
+++ react-vite/src/components/WantedMounts/MountListing.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom"
+import wowheadLogo from '../../wowhead-logo.png';
 import './WantedMounts.css';
 
 export default function MountListing({ mount }) {
 
   return (
     <div>
-      <NavLink className="wowhead-mount-link" to={`https://www.wowhead.com/mount/${mount.blizzId}`}>{mount.name}</NavLink>
+      <NavLink className="wowhead-mount-link" to={`https://www.wowhead.com/mount/${mount.blizzId}`}><img className="wowhead-logo" src={wowheadLogo} />{mount.name}</NavLink>
     </div>
   )
 }
