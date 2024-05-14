5211637b249f80d3b53437b5ba2587ac83e7cf32
Donald Roessler <me@donaldroessler.com>
2024-05-14 15:26:42 -0700

    Minor feed tweaks

--- react-vite/src/components/PostCard/PostCard.jsx
+++ react-vite/src/components/PostCard/PostCard.jsx
@@ -12,7 +12,9 @@ export default function PostCard({ post }) {
     <div className="post-card" onClick={clickOnPost}>
       <h4 className="post-card-mount-name">{post.featured_mount}</h4>
       <img className="post-card-image" src={post.post_image} />
+      <p className="post-card-author">by {post.author_name}</p>
       <p className="post-card-caption">{post.caption}</p>
+      <p className="post-card-comments-num">{post.comments_num != 1 ? `${post.comments_num} comments` : `${post.comments_num} comment`}</p>
     </div>
   )
 }
